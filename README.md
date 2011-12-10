c0ace2f719ac09c1ca6e67bb40dc02e07b513430
Sebastian Tschan <sebastian.tschan@gmail.com>
2011-12-11 05:44:52 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,84 @@
+# JavaScript MD5
+
+## Demo
+[JavaScript MD5 Demo](http://blueimp.github.com/JavaScript-MD5/)
+
+## Usage
+
+### Client-side
+Include the (minified) JavaScript [MD5](http://en.wikipedia.org/wiki/MD5) script in your HTML markup:
+
+```html
+<script src="md5.min.js"></script>
+```
+
+In your application code, calculate the ([hex](http://en.wikipedia.org/wiki/Hexadecimal)-encoded) [MD5](http://en.wikipedia.org/wiki/MD5) hash of a string by calling the **md5** method with the string as argument:
+
+```js
+var hash = md5("value"); // "2063c1608d6e0baf80249c42e2be5804"
+```
+
+### Server-side
+
+The following is an example how to use the JavaScript MD5 module on the server-side with [node.js](http://nodejs.org/).
+
+Create a new directory and add the **md5.js** file. Or alternatively, install the **blueimp-md5** package with [npm](http://npmjs.org/):
+
+```sh
+npm install blueimp-md5
+```
+
+Add a file **server.js** with the following content:
+
+```js
+require("http").createServer(function (req, res) {
+    // The md5 module exports the md5() function:
+    var md5 = require("./md5").md5,
+    // Use the following version if you installed the package with npm:
+    // var md5 = require("blueimp-md5").md5,
+        url  = require("url"),
+        query = url.parse(req.url).query;
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    // Calculate and print the MD5 hash of the url query:
+    res.end(md5(query));
+}).listen(8080, "localhost");
+console.log("Server running at http://localhost:8080/");
+```
+
+Run the application with the following command:
+
+```sh
+node server.js
+```
+
+## Requirements
+The JavaScript MD5 script has zero dependencies.
+
+## API
+
+Calculate the ([hex](http://en.wikipedia.org/wiki/Hexadecimal)-encoded) [MD5](http://en.wikipedia.org/wiki/MD5) hash of a given string value:
+
+```js
+var hash = md5("value"); // "2063c1608d6e0baf80249c42e2be5804"
+```
+
+Calculate the ([hex](http://en.wikipedia.org/wiki/Hexadecimal)-encoded) [HMAC](http://en.wikipedia.org/wiki/HMAC)-MD5 hash of a given string value and key:
+
+```js
+var hash = md5("value", "key"); // "01433efd5f16327ea4b31144572c67f6"
+```
+    
+Calculate the raw [MD5](http://en.wikipedia.org/wiki/MD5) hash of a given string value:
+
+```js
+var hash = md5("value", null, true);
+```
+
+Calculate the raw [HMAC](http://en.wikipedia.org/wiki/HMAC)-MD5 hash of a given string value and key:
+
+```js
+var hash = md5("value", "key", true);
+```
+
+## License
+The JavaScript MD5 script is released under the [MIT license](http://creativecommons.org/licenses/MIT/).
---
+++ test/test.js
@@ -0,0 +1,103 @@
+/*
+ * JavaScript MD5 Test 1.0
+ * https://github.com/blueimp/JavaScript-MD5
+ *
+ * Copyright 2011, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://creativecommons.org/licenses/MIT/
+ */
+
+/*global require */
+
+(function ($) {
+    'use strict';
+
+    var lifecycle = {
+            setup: function () {},
+            teardown: function () {}
+        },
+        nodeQunit = {
+            setup: function () {},
+            teardown: function () {}
+        };
+
+    if (!$.module) {
+        // Node.js QUnit compatible Testrunner:
+        nodeQunit = require('./node-qunit');
+        $.module = nodeQunit.module;
+        $.test = nodeQunit.test;
+        $.strictEqual = nodeQunit.assert.strictEqual;
+        $.md5 = require('../md5').md5;
+    }
+
+    nodeQunit.setup();
+
+    $.module('Hex-encoded MD5', lifecycle);
+
+    $.test('Creating hex-encoded MD5 hash of an ASCII value', function () {
+        $.strictEqual(
+            $.md5('value'),
+            '2063c1608d6e0baf80249c42e2be5804'
+        );
+    });
+
+    $.test('Creating hex-encoded MD5 hash of an UTF-8 value', function () {
+        $.strictEqual(
+            $.md5('日本'),
+            '4dbed2e657457884e67137d3514119b3'
+        );
+    });
+
+    $.module('Hex-encoded HMAC-MD5', lifecycle);
+
+    $.test('Creating hex-encoded HMAC-MD5 hash of an ASCII value and key', function () {
+        $.strictEqual(
+            $.md5('value', 'key'),
+            '01433efd5f16327ea4b31144572c67f6'
+        );
+    });
+
+    $.test('Creating hex-encoded HMAC-MD5 hash of an UTF-8 value and key', function () {
+        $.strictEqual(
+            $.md5('日本', '日本'),
+            'c78b8c7357926981cc04740bd3e9d015'
+        );
+    });
+
+    $.module('Raw MD5', lifecycle);
+
+    $.test('Creating raw MD5 hash of an ASCII value', function () {
+        $.strictEqual(
+            $.md5('value', null, true),
+            ' c\xc1`\x8dn\x0b\xaf\x80$\x9cB\xe2\xbeX\x04'
+        );
+    });
+
+    $.test('Creating raw MD5 hash of an UTF-8 value', function () {
+        $.strictEqual(
+            $.md5('日本', null, true),
+            'M\xbe\xd2\xe6WEx\x84\xe6q7\xd3QA\x19\xb3'
+        );
+    });
+
+    $.module('Raw HMAC-MD5', lifecycle);
+
+    $.test('Creating raw HMAC-MD5 hash of an ASCII value and key', function () {
+        $.strictEqual(
+            $.md5('value', 'key', true),
+            '\x01C>\xfd_\x162~\xa4\xb3\x11DW,g\xf6'
+        );
+    });
+
+    $.test('Creating raw HMAC-MD5 hash of an UTF-8 value and key', function () {
+        $.strictEqual(
+            $.md5('日本', '日本', true),
+            '\xc7\x8b\x8csW\x92i\x81\xcc\x04t\x0b\xd3\xe9\xd0\x15'
+        );
+    });
+
+    nodeQunit.teardown();
+
+}(this));
