e501057d06a52f11e0d3d40a5cccd8409387e64b
Sebastian Tschan <mail@blueimp.net>
2015-07-24 18:31:10 +0200

    Add license field to package definition(s).

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,11 +59,10 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-simple-mocha');
-    grunt.loadNpmTasks('grunt-mocha');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bump-build-git');
 
-    grunt.registerTask('test', ['jshint', 'simplemocha', 'mocha']);
+    grunt.registerTask('test', ['jshint', 'simplemocha']);
     grunt.registerTask('default', ['test', 'uglify']);
 
 };
